0a4f35f930041b44ccbe80977e0a327dec288303
bcarey <bcarey@LAPTOP-9IJC4N3L>
2017-12-08 16:31:39 -0500

    added amp support

--- src/main/java/com/careydevelopment/masculex/controller/PostController.java
+++ src/main/java/com/careydevelopment/masculex/controller/PostController.java
@@ -1,5 +1,7 @@
 package com.careydevelopment.masculex.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.careydevelopment.masculex.jpa.entity.Context;
 import com.careydevelopment.masculex.jpa.entity.Post;
-import com.careydevelopment.masculex.jpa.entity.Product;
 import com.careydevelopment.masculex.jpa.repository.ContextRepository;
 import com.careydevelopment.masculex.jpa.repository.PostRepository;
 
@@ -39,6 +40,39 @@ public class PostController {
         		model.addAttribute("post", post);
         		
                 return "post";	
+        	}        	
+    	}      
+    }
+    
+    
+    @RequestMapping(value = "/{contextName}/post/{slug}/amp", method=RequestMethod.GET)
+    public String postAmp(@PathVariable String contextName, @PathVariable String slug, Model model,
+    	HttpServletRequest request) {
+    	Context context = contextRepository.fetchByName(contextName);
+    	
+    	if (context == null) {
+    		return "404";
+    	} else {
+        	model.addAttribute("webContext", context);        	
+        	
+        	Post post = postRepository.fetchBySlug(slug);
+        	
+        	if (post == null) {
+        		return "404";
+        	} else {
+            	String url = request.getRequestURI();
+            	String noAmpUrl = url;
+            	
+            	int suffix = url.lastIndexOf("/amp");
+            	
+            	if (suffix > -1) {
+            		noAmpUrl = url.substring(0, suffix);
+            	}
+            	
+            	model.addAttribute("noAmpUrl",noAmpUrl);
+        		model.addAttribute("post", post);
+        		
+                return "postAmp";	
         	}        	
     	}      
     }
